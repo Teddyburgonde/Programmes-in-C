04b2a17e9e17b0238949d37be7cc12f676d29f78
Teddy Bandama <teddybandama@MacBook-Pro-de-Teddy.local>
2022-10-25 06:10:29 +0200

    use typedef

---
+++ use typedef.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAILLE_STRING 50
+#define TAILLE_PROMO 5
+#define NOTE_MOYENNE 10.0
+
+typedef struct ETUDIANT
+{
+    char nom[TAILLE_STRING];
+    char prenom[TAILLE_STRING];
+
+    float moyenne;
+
+}Etudiant;
+
+
+int main()
+{
+    //liste de la promo
+    Etudiant promo[TAILLE_PROMO] =
+    {
+        { "Mercier", "Arnaud", 13.63},
+        { "Leroux", "Thomas", 15.21},
+        { "Pegard", "Laurie", 5.98},
+        { "Minard", "Lydie", 12.56},
+        { "Snow", "John", 9.56}
+    };
+
+    //affichage des laureats
+    printf("Liste des loreats :\n");
+
+    for(int i = 0; i < TAILLE_PROMO; i++)
+    {
+        Etudiant candidat = promo[i];
+
+        if(candidat.moyenne >= NOTE_MOYENNE)
+            printf("\t%s %s: %.2f\n", candidat.prenom, candidat.nom, candidat.moyenne);
+    }
+
+    return 0;
+}
